40e43d1ecea7e8c5e01644a0c10c6c3b8682d386
Jonatan Knaul <jonatan.knaull@gmail.com>
2020-11-09 21:34:14 -0300

    Update image_example.js

--- pages/image_example.js
+++ pages/image_example.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import { Image } from 'react-native'
+import { View, Image } from 'react-native'
 
 const ImagesExample = () => (
-   <Image source = {require('C:/Users/Tutorialspoint/Desktop/NativeReactSample/logo.png')} />
+   <Image source = {{uri:'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png'}}
+   style = {{ width: 200, height: 200 }}
+   />
 )
 export default ImagesExample

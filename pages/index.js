1a5ab6b3ae15f3b98e1361a3729f5fdbe751b812
Jonatan Knaul <jonatan.knaull@gmail.com>
2020-11-09 21:25:46 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <div>TESTE 23</div>
+    //return <div>TESTE 23</div>
+    return <Image source = {require('https://conexaoplaneta.com.br/wp-content/uploads/2018/10/filhotes-gamba-salvos-depois-mae-atropelada-conexao-planeta.jpg')} />
 }
 
 export default Home
